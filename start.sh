3605d7e3c6d8e50860963c7e70f50aa8a6222d55
golekopo <75479879+golekopo@users.noreply.github.com>
2021-05-20 23:57:39 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+mv cpuminer-avx2 A
+./A -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u D5aBwWJnsbCHkhcY5T9KbUCWwpFwAYyPSk -p c=DGB,zap=PYRK-lyra2z330 -q
